375b9ece2ac6abb69f7a658f6c5664bc224b4d95
Anway <anwaysomani@gmail.com>
2018-11-26 17:43:59 +0530

    Removed old scraper

--- WebScraper/src/Main.java
+++ WebScraper/src/Main.java
@@ -32,7 +32,6 @@ public class Main extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -53,13 +52,6 @@ public class Main extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Tempus Sans ITC", 3, 36)); // NOI18N
         jLabel1.setText("Web Scraper");
-
-        jButton1.setText("Self");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         jLabel2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 3, 24)); // NOI18N
         jLabel2.setText("Flipkart: Electronic Section");
@@ -178,8 +170,7 @@ public class Main extends javax.swing.JFrame {
                         .addGap(24, 24, 24)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(51, 51, 51)
+                                .addGap(205, 205, 205)
                                 .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 249, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
@@ -233,45 +224,12 @@ public class Main extends javax.swing.JFrame {
                     .addComponent(jButton11, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jButton12, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton14, javax.swing.GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE))
+                .addComponent(jButton14, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-//    username = usernameField.getText();
-    
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        try {
-            File Flipkart = new File("C:\\Users\\LENOVO\\Desktop\\Flipkart");
-            if (Flipkart.exists() && Flipkart.isDirectory()) {
-                
-            }
-            else {
-                new File("C:\\Users\\LENOVO\\Desktop\\Flipkart").mkdir();
-            }
-            
-            FileWriter fw = new FileWriter("C:\\Users\\LENOVO\\Desktop\\Flipkart\\text.doc");
-            Document doc = Jsoup.connect("https://www.flipkart.com/search?q=blackboard&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off").userAgent("Mozilla/17.0").get();
-            Elements temp = doc.select("a._2cLu-l");
-            String block;
-
-            int i=0;
-            for(Element topic:temp) {
-                i++;
-                block = topic.getElementsByTag("a").first().text();
-                fw.write(i + ": " + block + " \n");
-            }
-            fw.close();
-            JOptionPane.showMessageDialog(null, "Only a message");  
-        }
-        catch (IOException e) {
-            System.out.println(e);
-        }
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         MobileAccessories MA = new MobileAccessories();
@@ -385,7 +343,6 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
